87cdc0a5bba593c8d4f60993c7a5c7bd54883ecb
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-06 00:10:52 +0530

    Update start.py

--- ElevenHost/modules/start.py
+++ ElevenHost/modules/start.py
@@ -1,1 +1,14 @@
+from ElevenHost import *
+from pyrogram import *
+from ElevenHost.api import *
 
+api = OneApi()
+
+@app.on_message(filters.command('start'))
+async def start(_, message):
+  if await api.exists(message.from_user.id): pass
+  else: # Welcome new users heHe
+    await message.reply("hm looks like new user")
+    return
+  # ------------------------------------------------
+  await message.reply("I know you before")
