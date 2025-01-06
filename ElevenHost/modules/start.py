2f57f2bde9fcf7a4584aff948b07ff72cd63d76f
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-06 12:14:12 +0530

    Update start.py

--- ElevenHost/modules/start.py
+++ ElevenHost/modules/start.py
@@ -3,6 +3,8 @@ from pyrogram import *
 from ElevenHost.api import *
 
 api = OneApi()
+# _-_-_-_ Connect ws |
+api.connect()
 
 @app.on_message(filters.command('start'))
 async def start(_, message):
