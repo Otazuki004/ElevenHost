723d5f669264d8aee96d9cc644945a445a282b52
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-06 12:22:26 +0530

    Update OneApi.py

--- ElevenHost/api/OneApi.py
+++ ElevenHost/api/OneApi.py
@@ -1,2 +1,10 @@
+import asyncio
+from ElevenHost import OneApiUrl
 
 class OneApi:
+  """This class is created for communicate with OneApi"""
+  def connect():
+    return True
+  async def exists(user_id):
+    return False
+  
