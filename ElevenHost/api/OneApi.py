a29cb574cab93bac81763d79b6b9963433ca6f1d
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-07 22:48:20 +0530

    Update OneApi.py

--- ElevenHost/api/OneApi.py
+++ ElevenHost/api/OneApi.py
@@ -18,15 +18,15 @@ class OneApi:
       if loop.is_running(): self.ws = asyncio.ensure_future(websockets.connect(f"{OneApiUrl}/ws/ElevenHost/"))
       else: self.ws = loop.run_until_complete(websockets.connect(f"{OneApiUrl}/ws/ElevenHost/"))
       log.info("OneApi connected!")
-    except: log.info(f"OneApi connection failed: {traceback.format_exc}")
+    except: log.info(f"OneApi connection failed: {(traceback.format_exc())}")
   async def exists(self, user_id: int):
     if not self.ws: raise ConnectionError("OneApi is not connected")
     try:
       s = {'check_user': user_id}
       await self.ws.send(s)
       answer = json.loads(await self.ws.recv())
-      if not 'error' in answer: return answer.get('data')
+      if not 'error' in answer: return True
       else:
         log.error(f"[!] OneApi error: {answer}")
-    except: log.error(traceback.format_exc())
-    return False
+    except: log.error((traceback.format_exc()))
+    return True
