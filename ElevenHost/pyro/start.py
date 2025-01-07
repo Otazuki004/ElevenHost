1d7b7a0252c7730ead36678eb82714af944a34e9
Aᴋɪᴛᴏ <varshakoli638@gmail.com>
2025-01-07 21:54:05 +0530

    Update start.py

--- ElevenHost/pyro/start.py
+++ ElevenHost/pyro/start.py
@@ -1,13 +1,14 @@
 from ElevenHost import *
 from pyrogram import *
 from ElevenHost.api import *
-
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton 
 
 @app.on_message(filters.command('start'))
 async def start(_, message):
-  if await api.exists(message.from_user.id): pass
-  else: # Welcome new users heHe
-    await message.reply("hm looks like new user")
-    return
-  # ------------------------------------------------
-  await message.reply("I know you before")
+    if await api.exists(message.from_user.id): pass
+    else: # Welcome new users heHe
+        await message.reply_photo(photo="otazuki.jpg", caption="| 𝗘𝗟𝗘𝗩𝗘𝗡 𝗛𝗢𝗦𝗧 |\n\n🍃 We provide the most reliable 🧡 and high-performance 🌩️ hosting solutions with a focus on simplicity and ease of use.\nEnjoy fast and secure hosting with unlimited potential! 🗝️ (°ᴗ°) 🥀✨\n\n🦋 Channel: @ElevenHost\n👾 Support: @ElevenHostSupport", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("hi", callback_data="hi")]))
+        return
+    # ------------------------------------------------
+    await message.reply_photo(photo="otazuki.jpg", caption="| 𝗘𝗟𝗘𝗩𝗘𝗡 𝗛𝗢𝗦𝗧 |\n\n🍃 We provide the most reliable 🧡 and high-performance 🌩️ hosting solutions with a focus on simplicity and ease of use.\nEnjoy fast and secure hosting with unlimited potential! 🗝️ (°ᴗ°) 🥀✨\n\n🦋 Channel: @ElevenHost\n👾 Support: @ElevenHostSupport", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Heya", callback_data="heya")]))
