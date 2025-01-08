26050991516ae7f3cf47610e95a8346694ea9023
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-08 14:55:08 +0530

    Update logs.py

--- ElevenHost/pyro/logs.py
+++ ElevenHost/pyro/logs.py
@@ -6,6 +6,6 @@ async def logs(_, message):
   run_logs = await run("tail log.txt")
   await message.reply_text(f"```shell\n{run_logs}```")
 
-@app.on_message(filters.command(["flogs", "flog"]) filters.user(DEVS))
+@app.on_message(filters.command(["flogs", "flog"]) & filters.user(DEVS))
 async def flogs(_, message):
   await message.reply_document(document='log.txt')
