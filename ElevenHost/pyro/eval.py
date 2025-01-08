79245aeca0743cfb8308137fb360608cd4d52f33
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-08 14:30:20 +0530

    Update eval.py

--- ElevenHost/pyro/eval.py
+++ ElevenHost/pyro/eval.py
@@ -1,4 +1,4 @@
-# ©️ @ParadopiaXD | @YoursSage
+# ©️ @Hyper_Speed0 & @ParadopiaxD
 
 import io
 import sys
@@ -7,14 +7,16 @@ from ElevenHost import app
 from datetime import datetime
 from pyrogram import filters
 from pyrogram.types import Message
+import aiofiles
 
 async def aexec(code, app, msg):
+    m, from_user, r = msg, msg.from_user, msg.reply_to_message
     exec(
-        "async def __aexec(app, msg): "
+        "async def __otazuki_run(app, msg, m, r, from): "
         + "\n p = print"
         + "".join(f"\n {l_}" for l_ in code.split("\n"))
     )
-    return await locals()["__aexec"](app, msg)
+    return await locals()["__otazuki_run"](app, msg, m, r, from_user)
     
 @app.on_message(filters.command("eval"))
 @app.on_edited_message(filters.command("eval"))
@@ -41,7 +43,9 @@ async def runPyro_Funcs(app:app, msg:Message) -> None:
     eoac = datetime.now()
     runcs = (eoac - soac).microseconds / 1000
     oucode = f"📎 ᴄᴏᴅᴇ:\n<pre>{code[1]}</pre>\n📒 ᴏᴜᴛᴘᴜᴛ:\n<pre>{evason}</pre>\n✨ ᴛɪᴍᴇ ᴛᴀᴋᴇɴ: {runcs}ᴍɪʟɪsᴇᴄᴏɴᴅ"
-    if len(oucode) > 10000000:
-        await message.edit("⚠️ ᴏᴜᴛᴘᴜᴛ ᴛᴏᴏ ʟᴏɴɢ...")
+    if len(oucode) > 4000:
+        async with aiofiles.open('eval.txt', mode='w') as f:
+            await f.write(oucode)
+        await message.reply_document('eval.txt')
     else:
         await message.edit(oucode)
