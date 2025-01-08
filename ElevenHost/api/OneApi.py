a6b7e79d7f8f84a2b1ba09580b39644731e2ed48
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-08 22:41:30 +0530

    Update OneApi.py

--- ElevenHost/api/OneApi.py
+++ ElevenHost/api/OneApi.py
@@ -9,21 +9,23 @@ log = logging.getLogger("OneApi")
 class OneApi:
   def __init__(self):
     self.ws = None
-  def connect(self):
+    exists_ws = None
+  async def connect_routes(self):
     from .. import OneApiUrl
+    self.exists_ws = await websockets.connect(f"{OneApiUrl}/ws/exists/")
+    self.ws = True
+  def connect(self):
     if self.ws: log.error("You've already connected with OneApi")
     log.info("[^•^] Connecting with OneApi")
     try:
       loop = asyncio.get_event_loop()
-      if loop.is_running(): self.ws = asyncio.ensure_future(websockets.connect(f"{OneApiUrl}/ws/ElevenHost/"))
-      else: self.ws = loop.run_until_complete(websockets.connect(f"{OneApiUrl}/ws/ElevenHost/"))
+      if loop.is_running(): asyncio.ensure_future(self.connect_routes())
+      else: loop.run_until_complete(self.connect_routes())
       log.info("OneApi connected!")
     except: log.info(f"OneApi connection failed: {(traceback.format_exc())}")
   async def exists(self, user_id: int):
-    return True
-    if not self.ws: raise ConnectionError("OneApi is not connected")
+    if not self.exists_ws: raise ConnectionError("OneApi is not connected")
     try:
-      s = {'check_user': user_id}
       await self.ws.send(s)
       answer = json.loads(await self.ws.recv())
       if not 'error' in answer: return True
