40a9e63076ed7194e52e513db21af36817628467
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-08 14:50:13 +0530

    Update __init__.py

--- ElevenHost/__init__.py
+++ ElevenHost/__init__.py
@@ -35,6 +35,7 @@ API_HASH = os.environ.get("API_HASH") or VAR_API_HASH
 HANDLER = [".","~","!","$","#"]
 TOKEN = os.environ.get("TOKEN") or VAR_TOKEN
 MY_VERSION = 1.2
+DEVS = os.environ.get("DEVS") or VAR_DEVS
 # _______________________________________
 if not API_ID or not API_HASH or not TOKEN or not OneApi:
   raise ValueError("Bro thought he can run anything lol, i mean you forgot some vars put on variables.py")
