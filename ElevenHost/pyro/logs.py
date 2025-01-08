d16625fca605c682756019cfbd8f40e5159aac7c
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-08 14:54:35 +0530

    Update logs.py

--- ElevenHost/pyro/logs.py
+++ ElevenHost/pyro/logs.py
@@ -1,35 +1,11 @@
-import io
 from pyrogram import *
-from ElevenHost import app
-import traceback
-from subprocess import getoutput as run
-from pyrogram.enums import ChatAction
+from ElevenHost import *
 
-@app.on_message(filters.command(["logs", "log"]))
+@app.on_message(filters.command(["logs", "log"]) & filters.user(DEVS))
 async def logs(_, message):
-    if message.from_user.id == OWNER_ID or message.from_user.id in CO_OWNER_ID:
-        print("")
-    else:
-        return
-    run_logs = run("tail log.txt")
-    text = await message.reply_text("`Getting logs...`")
-    await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
-    await message.reply_text(f"```shell\n{run_logs}```")
-    await text.delete()
+  run_logs = await run("tail log.txt")
+  await message.reply_text(f"```shell\n{run_logs}```")
 
-
-@app.on_message(filters.command(["flogs", "flog"], prefixes=prefix))
-async def logs(_, message):
-    if message.from_user.id == OWNER_ID or message.from_user.id in CO_OWNER_ID:
-        print("")
-    else:
-        return
-    run_logs = run("cat log.txt")
-    text = await message.reply_text("`Sending Full logs...`")
-    await bot.send_chat_action(message.chat.id, ChatAction.UPLOAD_DOCUMENT)
-    with io.BytesIO(str.encode(run_logs)) as logs:
-        logs.name = "log.txt"
-        await message.reply_document(
-            document=logs,
-        )
-    await text.delete()
+@app.on_message(filters.command(["flogs", "flog"]) filters.user(DEVS))
+async def flogs(_, message):
+  await message.reply_document(document='log.txt')
