ea73b61891243062a87bcff97aa53b07df7411f3
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-09 15:15:43 +0530

    Update start.py

--- ElevenHost/pyro/start.py
+++ ElevenHost/pyro/start.py
@@ -8,7 +8,8 @@ import logging
 @app.on_message(filters.command('start'))
 async def start(_, message):
     if not await api.exists(message.from_user.id):
-        await message.reply_photo(photo="https://i.imgur.com/rAGtzwy.jpeg", caption="| 𝗘𝗟𝗘𝗩𝗘𝗡 𝗛𝗢𝗦𝗧 |\n\n🍃 We provide the most reliable 🧡 and high-performance 🌩️ hosting solutions with a focus on simplicity and ease of use.\nEnjoy fast and secure hosting with unlimited potential! 🗝️ (°ᴗ°) 🥀✨\n\n🦋 Channel: @ElevenHost\n👾 Support: @ElevenHostSupport", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("hi", callback_data="hi")]]))
-        return
+        a = await message.reply("Registering you...")
+        await api.create_user(message.from_user.first_name, message.from_user.id)
+        await a.delete()
     # ------------------------------------------------
     await message.reply_photo(photo="https://i.imgur.com/rAGtzwy.jpeg", caption="| 𝗘𝗟𝗘𝗩𝗘𝗡 𝗛𝗢𝗦𝗧 |\n\n🍃 We provide the most reliable 🧡 and high-performance 🌩️ hosting solutions with a focus on simplicity and ease of use.\nEnjoy fast and secure hosting with unlimited potential! 🗝️ (°ᴗ°) 🥀✨\n\n🦋 Channel: @ElevenHost\n👾 Support: @ElevenHostSupport", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Heya", callback_data="heya")]]))
