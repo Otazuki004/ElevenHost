0079d40875ba2f2552fca7fd0851393eb4fea679
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-09 13:11:11 +0530

    Update OneApi.py

--- ElevenHost/api/OneApi.py
+++ ElevenHost/api/OneApi.py
@@ -1,5 +1,4 @@
-import asyncio
-import websockets
+import httpx
 import logging
 import traceback
 import json
@@ -7,29 +6,29 @@ import json
 log = logging.getLogger("OneApi")
 
 class OneApi:
-  def __init__(self):
-    self.ws = None
-    exists_ws = None
-  async def connect_routes(self):
-    from .. import OneApiUrl
-    self.exists_ws = await websockets.connect(f"{OneApiUrl}/ws/exists/")
-    self.ws = True
+  def __init__(self, api_url):
+    self.connected = False
+    self.url = api_url
   def connect(self):
-    if self.ws: log.error("You've already connected with OneApi")
+    if self.connected: return log.error("You've already connected with OneApi")
     log.info("[^•^] Connecting with OneApi")
     try:
-      loop = asyncio.get_event_loop()
-      if loop.is_running(): asyncio.ensure_future(self.connect_routes())
-      else: loop.run_until_complete(self.connect_routes())
+      self.connected = True
       log.info("OneApi connected!")
-    except: log.info(f"OneApi connection failed: {(traceback.format_exc())}")
+    except:
+      log.error(traceback.format_exc())
   async def exists(self, user_id: int):
-    if not self.exists_ws: raise ConnectionError("OneApi is not connected")
+    if not self.connected: raise ConnectionError("OneApi isn't connected")
     try:
-      await self.ws.send(s)
-      answer = json.loads(await self.ws.recv())
-      if not 'error' in answer: return True
-      else:
-        log.error(f"[!] OneApi error: {answer}")
-    except: log.error((traceback.format_exc()))
-    return True
+      data = {"user_id": user_id}
+      async with httpx.AsyncClient() as client:
+        response = await client.post(f'{self.url}/exists/', json=data)
+        if response.status_code == 200:
+          return True
+        elif response.status_code == 404:
+          return False
+        else:
+          log.error(f"[!] OneApi error: {response.status_code}: {response.text}")
+    except:
+      log.error(traceback.format_exc())
+    return False
