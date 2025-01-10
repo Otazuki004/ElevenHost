9ec13ebb7a1fb0cea11cb2e4b6187196c9edf9f2
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-10 20:31:15 +0530

    Update sh.py

--- ElevenHost/pyro/sh.py
+++ ElevenHost/pyro/sh.py
@@ -4,11 +4,11 @@ import aiofiles
 
 @app.on_message(filters.command('sh'))
 async def sh(_, message):
-  a = await message.reply("Processing...")
   if len(message.command) < 2:
-    return await message.reply("Text too short")
+    return await message.reply("ᴄᴏᴅᴇ ɴᴏᴛ ғᴏᴜɴᴅ !")
+  a = await message.reply("Processing...")
   cmd = message.text.split(None, 1)[1]
-  results = await run(cmd)
+  results = await run(cmd) or "ɴᴏ ᴏᴜᴛᴘᴜᴛ"
   if len(results) > 4000:
     async with aiofiles.open('shell.txt', mode='w') as uwu:
       await uwu.write(results)
