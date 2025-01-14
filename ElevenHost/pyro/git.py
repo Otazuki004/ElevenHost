335d8d3d4eafc62b643ea548ebd95300e5ef70a6
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-14 13:47:38 +0530

    Update git.py

--- ElevenHost/pyro/git.py
+++ ElevenHost/pyro/git.py
@@ -24,6 +24,17 @@ Name: {user.get('git')}
 Bio: {bio}
 Followers: {followers}
 Following: {following}"""
-  btn = InlineKeyboardMarkup([[InlineKeyboardButton("Disconnect", callback_data=f"dis_github_{message.from_user.id}")]])
+  btn = InlineKeyboardMarkup([[InlineKeyboardButton("Disconnect", callback_data=f"dis_github")]])
   await love.delete()
-  await message.reply(txt, reply_markup=btn)
+  await message.reply_photo("http://ibb.co/0nTck0T", caption=txt, reply_markup=btn)
+
+@app.on_callback_query(filters.regex("dis_github"))
+async def disconnect_git(_, query):
+  user_id = query.from_user.id
+  if not await api.exists(user_id): return await query.answer("❌ You are not registered yet. Please use /start to register your account.", show_alert=True)
+  ohk = await api.disconnect_git(user_id)
+  if ohk:
+    await query.message.delete()
+    await app.send_message(query.message.chat.id, "Successfully disconnected your github.")
+  else:
+    await query.message.edit("Failed to disconnect your GitHub")
