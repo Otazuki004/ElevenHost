0d7faddb7a2ee1f2cf3c77ed0119f1f8a9ef5949
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-14 19:06:17 +0530

    Update __init__.py

--- ElevenHost/api/methods/__init__.py
+++ ElevenHost/api/methods/__init__.py
@@ -2,10 +2,15 @@ from .user_info import UserInfo
 from .get_repos import GetRepos
 from .delete_user import DeleteUser
 from .disconnect_git import DisconnectGit
+from .set_repo import SetRepo
+from .project_info import ProjectInfo
+
 class Methods(
   UserInfo,
   GetRepos,
   DeleteUser,
   DisconnectGit,
+  SetRepo,
+  ProjectInfo,
 ):
   pass
