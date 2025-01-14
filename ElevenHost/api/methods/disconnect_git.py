3348f3a6094f5eb31293d0ae216d38aedf3216f6
Otazuki <115762083+Otazuki004@users.noreply.github.com>
2025-01-14 15:21:42 +0530

    Update disconnect_git.py

--- ElevenHost/api/methods/disconnect_git.py
+++ ElevenHost/api/methods/disconnect_git.py
@@ -3,4 +3,13 @@ import traceback
 
 class DisconnectGit:
   async def disconnect_git(self, user_id: int):
-    return True
+    if not self.connected: raise ConnectionError("OneApi isn't connected")
+    try:
+      data = {"user_id": user_id}
+      async with httpx.AsyncClient() as client:
+        response = await client.post(f'{self.url}/disconnect_git/', json=data)
+        if response.status_code == 200:
+          return True
+    except:
+      self.log.error(traceback.format_exc())
+    return False
